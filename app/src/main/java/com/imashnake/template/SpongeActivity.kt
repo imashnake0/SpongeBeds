1fd7105b5c7ac2ee5e869819f3ea4eb78905fd73
imashnake0 <krpaluru@uwaterloo.ca>
2026-06-23 01:33:10 -0400

    create new activity and navigate to it

--- app/src/main/java/com/imashnake/template/SpongeActivity.kt
+++ app/src/main/java/com/imashnake/template/SpongeActivity.kt
@@ -1,39 +1,27 @@
 package com.imashnake.template
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import com.imashnake.template.ui.theme.AndroidTemplateTheme
 
-class MainActivity : ComponentActivity() {
+class SpongeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val context = LocalContext.current
-
             AndroidTemplateTheme {
                 Box(
                     contentAlignment = Alignment.Center,
                     modifier = Modifier.fillMaxSize()
                 ) {
-                    Button(
-                        onClick = {
-                            val intent = Intent(context, SpongeActivity::class.java)
-                            context.startActivity(intent)
-                        }
-                    ) {
-                        Text(text = "Navigate to SpongeActivity")
-                    }
+                    Text("This is SpongeActivity")
                 }
             }
         }
